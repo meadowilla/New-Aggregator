f9efb3d7ea99ce1720e4fcb03fbe8bbecb199452
for-Ely <kaneki21174@gmail.com>
2024-04-30 17:45:31 +0700

    adjust a bit, not change things

--- src/main/java/com/newsagg/spring/GetData.java
+++ src/main/java/com/newsagg/spring/GetData.java
@@ -1,13 +1,11 @@
 package com.newsagg.spring;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import org.apache.commons.csv.*;
-
-
-
 
 public class GetData {
     public List<Data> getData() {
@@ -16,22 +14,22 @@ public class GetData {
         try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
             br.readLine();
             Iterable<CSVRecord> csvParser = CSVFormat.DEFAULT.parse(br);
-                for (CSVRecord csvRecord : csvParser) {
-                    Data data = new Data();
-                    data.setLink(csvRecord.get(0));
-                    data.setWebsite(csvRecord.get(1));
-                    data.setTitle(csvRecord.get(2));
-                    data.setDescription(csvRecord.get(3));
-                    data.setAuthor(csvRecord.get(4));
-                    data.setDate(csvRecord.get(5));
-                    data.setType(csvRecord.get(6));
-                    data.setKeywords(csvRecord.get(7)); 
-                    d.add(data);   
-                }
-        
+            for (CSVRecord csvRecord : csvParser) {
+                Data data = new Data();
+                data.setLink(csvRecord.get(0));
+                data.setWebsite(csvRecord.get(1));
+                data.setTitle(csvRecord.get(2));
+                data.setDescription(csvRecord.get(3));
+                data.setAuthor(csvRecord.get(4));
+                data.setDate(csvRecord.get(5));
+                data.setType(csvRecord.get(6));
+                data.setKeywords(csvRecord.get(7));
+                d.add(data);
+            }
+
         } catch (IOException e) {
             e.printStackTrace();
         }
-    return d;
+        return d;
     }
 }
