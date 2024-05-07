f926e2b291ef8581ca6bde54a6ef3b969eb539a6
for-Ely <kaneki21174@gmail.com>
2024-05-07 15:41:25 +0700

    toi tram cam

---
+++ src/python/Flask.py
@@ -0,0 +1,13 @@
+import json
+import Indexing
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+@app.route('/search', methods=['GET'])
+def search():
+    keyword = request.args.get('keyword')
+    result = Indexing.search(keyword)
+    return result
+if __name__ == '__main__':
+    app.run(debug=True)
