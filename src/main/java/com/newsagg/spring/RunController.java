7e2e9fade057d1fcabd1c59a5e8d13932e9d76d1
for-Ely <kaneki21174@gmail.com>
2024-05-15 23:15:12 +0700

    Clean up code

--- src/main/java/com/newsagg/spring/RunController.java
+++ src/main/java/com/newsagg/spring/RunController.java
@@ -2,13 +2,11 @@ package com.newsagg.spring;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.client.RestTemplate;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -19,30 +17,16 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @Controller
 @SessionAttributes("searchkey")
 public class RunController {
-    @GetMapping("/test")
-    public String test(ModelMap model) {
-        GetData gd = new GetData();
-        model.addAttribute("news", gd.getData());
-        return "test";
-    }
-    
-    @GetMapping("/hello")
-    public String hello(ModelMap model) {
-        GetData gd = new GetData();
-        model.addAttribute("news", gd.getData());
-        return "index";
-    }
     @GetMapping("/home")
     public String showSearchForm(ModelMap model) {
     GetData getData = new GetData();
     model.addAttribute("news", getData.getData());
-        return "search";
+        return "home";
         }
-
     @PostMapping("/home")
     public String performSearch(@RequestParam(value="searchkey", required = false) String searchkey,
-                                @RequestParam(value = "year", defaultValue = "") String year,
-                                @RequestParam(value = "month", defaultValue = "") String month,
+                                @RequestParam(value = "year", defaultValue = "year") String year,
+                                @RequestParam(value = "month", defaultValue = "month") String month,
                                 ModelMap model) throws JsonMappingException, JsonProcessingException {
          
         RestTemplate restTemplate = new RestTemplate();
@@ -65,45 +49,22 @@ public class RunController {
                 data.setImage(node.get(8).asText());
                 dataList.add(data);
         }
+
+        GetData getData = new GetData();
+        List<String> yearlist = new ArrayList<String>();
+        for (Data data : getData.getData()) {
+            if (!yearlist.contains(data.getPublishedDate().substring(0, 4))) {
+                yearlist.add(data.getPublishedDate().substring(0, 4));
+            }
+        }
+
+        model.addAttribute("yearlist", yearlist);
         model.addAttribute("searchkey", searchkey);
         model.addAttribute("year", year);
+        model.addAttribute("month", month);
         model.addAttribute("resultCount", dataList.size());
         model.addAttribute("result", dataList);
 
-
-        return "search";
-    }
-
-
-    @PostMapping("/test")
-    public String test(@SessionAttribute("searchkey") String searchkey,
-                        @RequestParam("year") String year,
-                        @RequestParam("month") String month,
-                        ModelMap model) throws JsonMappingException, JsonProcessingException {
-        RestTemplate restTemplate = new RestTemplate();
-        String result = restTemplate.getForObject("http://localhost:5000/search?searchkey=" + searchkey + "&year=" + year + "&month=" + month, String.class);
-        ObjectMapper objectMapper = new ObjectMapper();
-        JsonNode jsonNode = objectMapper.readTree(result);
-        
-        List<Data> dataList = new ArrayList<Data>();
-        for (JsonNode node : jsonNode) {
-                Data data = new Data();
-                data.setLink(node.get(0).asText() );
-                data.setSourceWebsite(node.get(1).asText());
-                data.setWebsite(node.get(2).asText());
-                data.setTitle(node.get(3).asText());
-                data.setDescription(node.get(4).asText());
-                data.setAuthor(node.get(5).asText());
-                data.setPublishedDate(node.get(6).asText());
-                data.setType(node.get(7).asText());
-                data.setImage(node.get(8).asText());
-                dataList.add(data);
-        }
-        model.addAttribute("searchkey", searchkey);
-        model.addAttribute("resultCount", dataList.size());
-        model.addAttribute("result", dataList);
-        return "search";
-
-    }
-    
+        return "home";
+    }    
 }
--- src/python/KeyYearMonthSearch.py
+++ src/python/KeyYearMonthSearch.py
@@ -96,9 +96,9 @@ def key_year_search(search_key, year):
     return output_documents
  
 def key_year_month_search(search_key, year, month):
-    if month =="" and year == "":
+    if month =="month" and year == "year":
         return key_search(search_key)
-    elif month == "":
+    elif month == "month":
         return key_year_search(search_key, year)
     else:
         file_path = 'src\\main\\resources\\test_year_search.csv' # Relative Path to your csv file
