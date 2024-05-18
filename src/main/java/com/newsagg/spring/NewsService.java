4e3cec78a8177f8cfb472f770b922c7623fb297c
for-Ely <kaneki21174@gmail.com>
2024-05-18 21:41:22 +0700

    minimize code from Linh advise

--- src/main/java/com/newsagg/spring/NewsService.java
+++ src/main/java/com/newsagg/spring/NewsService.java
@@ -3,140 +3,109 @@ package com.newsagg.spring;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-
 @Service
 public class NewsService {
-    
+
+    private final GetData getData = new GetData();
+    private final RestTemplate restTemplate = new RestTemplate();
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
     public List<Data> getAllData() {
-        GetData getData = new GetData();
         return getData.getData();
     }
 
     public List<String> getWebsiteList(List<Data> dataList) {
-        List<String> websiteList = new ArrayList<>();
-        for (Data data : dataList) {
-            if (!websiteList.contains(data.getSourceWebsite())) {
-                websiteList.add(data.getSourceWebsite());
-            }
-        }
-        return websiteList;
+        return dataList.stream()
+                .map(Data::getSourceWebsite)
+                .distinct()
+                .collect(Collectors.toList());
     }
 
     public List<Data> getDataForSpecificWebsite(String website, List<Data> dataList) {
-        if (website.equals("All")) {
-            return dataList;
-        } else {
-            List<Data> dataForSpecificWebsite = new ArrayList<>();
-            for (Data data : dataList) {
-                if (data.getSourceWebsite().equals(website)) {
-                    dataForSpecificWebsite.add(data);
-                }
-            }
-            return dataForSpecificWebsite;
+        return website.equals("All") ? dataList :
+                dataList.stream()
+                        .filter(data -> data.getSourceWebsite().equals(website))
+                        .collect(Collectors.toList());
+    }
+public List<Data> getSearchResults(String searchKey, String year, String month) throws JsonProcessingException {
+    List<Data> dataList = new ArrayList<>();
+    
+    if (searchKey == null || searchKey.isEmpty() || searchKey.equals("All")) {
+        dataList = getAllData();
+    } else {
+        String result = restTemplate.getForObject(
+                "http://localhost:5000/search?searchkey=" + searchKey + "&year=" + year + "&month=" + month,
+                String.class);
+        JsonNode jsonNode = objectMapper.readTree(result);
+        
+        for (JsonNode node : jsonNode) {
+            Data data = new Data();
+            data.setLink(node.get(0).asText());
+            data.setSourceWebsite(node.get(1).asText());
+            data.setWebsite(node.get(2).asText());
+            data.setTitle(node.get(3).asText());
+            data.setDescription(node.get(4).asText());
+            data.setAuthor(node.get(5).asText());
+            data.setPublishedDate(node.get(6).asText());
+            data.setType(node.get(7).asText());
+            data.setImage(node.get(8).asText());
+            dataList.add(data);
         }
     }
+    
+    return dataList;
+}
 
-    public List<Data> getSearchResults(String searchKey, String year, String month) throws JsonProcessingException {
-        List<Data> dataList = new ArrayList<>();
-        if (searchKey == null || searchKey.isEmpty() || searchKey.equals("All")) {
-            dataList = getAllData();
-            return dataList;
-        }
-        else {
-            RestTemplate restTemplate = new RestTemplate();
-            String result = restTemplate.getForObject(
-                    "http://localhost:5000/search?searchkey=" + searchKey + "&year=" + year + "&month=" + month,
-                    String.class);
-            ObjectMapper objectMapper = new ObjectMapper();
-            JsonNode jsonNode = objectMapper.readTree(result);
-            for (JsonNode node : jsonNode) {
-                Data data = new Data();
-                data.setLink(node.get(0).asText());
-                data.setSourceWebsite(node.get(1).asText());
-                data.setWebsite(node.get(2).asText());
-                data.setTitle(node.get(3).asText());
-                data.setDescription(node.get(4).asText());
-                data.setAuthor(node.get(5).asText());
-                data.setPublishedDate(node.get(6).asText());
-                data.setType(node.get(7).asText());
-                data.setImage(node.get(8).asText());
-                dataList.add(data);
-            }
-            return dataList;
-        }
-    }
 
     public List<String> getDistinctYears(List<Data> dataList) {
-        List<String> yearList = new ArrayList<>();
-        for (Data data : dataList) {
-            String year = data.getPublishedDate().substring(0, 4);
-            if (!yearList.contains(year)) {
-                yearList.add(year);
-            }
-        }
-        yearList.sort(Comparator.reverseOrder());
-        return yearList;
+        return dataList.stream()
+                .map(data -> data.getPublishedDate().substring(0, 4))
+                .distinct()
+                .sorted(Comparator.reverseOrder())
+                .collect(Collectors.toList());
     }
 
     public List<String> getDistinctAuthors(List<Data> dataList) {
-        List<String> writerList = new ArrayList<>();
-        for (Data data : dataList) {
-            if (!writerList.contains(data.getAuthor())) {
-                writerList.add(data.getAuthor());
-            }
-        }
-        writerList.sort(Comparator.naturalOrder());
-        return writerList;
+        return dataList.stream()
+                .map(Data::getAuthor)
+                .distinct()
+                .sorted()
+                .collect(Collectors.toList());
     }
 
     public List<String> getDistinctTypes(List<Data> dataList) {
-        List<String> typeList = new ArrayList<>();
-        for (Data data : dataList) {
-            if (!typeList.contains(data.getType())) {
-                typeList.add(data.getType());
-            }
-        }
-        typeList.sort(Comparator.naturalOrder());
-        return typeList;
+        return dataList.stream()
+                .map(Data::getType)
+                .distinct()
+                .sorted()
+                .collect(Collectors.toList());
     }
 
     public List<Data> filterDataByAttribute(List<Data> dataList, String attribute, String value) {
-        List<Data> filteredData = new ArrayList<>();
-        for (Data data : dataList) {
-            switch (attribute) {
-                case "website":
-                    if (data.getSourceWebsite().equals(value)) {
-                        filteredData.add(data);
+        return dataList.stream()
+                .filter(data -> {
+                    switch (attribute) {
+                        case "website":
+                            return data.getSourceWebsite().equals(value);
+                        case "author":
+                            return data.getAuthor().equals(value);
+                        case "type":
+                            return data.getType().equals(value);
+                        case "year":
+                            return data.getPublishedDate().substring(0, 4).equals(value);
+                        case "month":
+                            return data.getPublishedDate().substring(5, 7).equals(value);
+                        default:
+                            return false;
                     }
-                    break;
-                case "author":
-                    if (data.getAuthor().equals(value)) {
-                        filteredData.add(data);
-                    }
-                    break;
-                case "type":
-                    if (data.getType().equals(value)) {
-                        filteredData.add(data);
-                    }
-                    break;
-                case "year":
-                    if (data.getPublishedDate().substring(0, 4).equals(value)) {
-                        filteredData.add(data);
-                    }
-                    break;
-                case "month":
-                    if (data.getPublishedDate().substring(5, 7).equals(value)) {
-                        filteredData.add(data);
-                    }
-                    break;
-            }
-        }
-        return filteredData;
+                })
+                .collect(Collectors.toList());
     }
 }
